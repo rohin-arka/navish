29d59f769163ce0d6a4ea21345da6d95614ecd58
Sujit Shakya <sshakya.mail@gmail.com>
2017-12-05 01:44:21 +0545

    Install command added in navi script.

--- install-navi.sh
+++ install-navi.sh
@@ -2,17 +2,16 @@
 
 # creates navi folder
 mkdir ~/.navi
+sudo chmod -R 777 ~/.navi
 cp bin/navi.sh ~/.navi/
 
 # shell = $SHELL
 if [ "$SHELL"="/bin/zsh" ]; then
   echo "alias navi='$HOME/.navi/navi.sh'" >> ~/.zshrc
   zsh
-  source .zshrc
   print "Written to .zshrc"
 else
   echo "alias navi='$HOME/.navi/navi.sh'" >> ~/.bashrc
   bash
-  source .bashrc
   echo "Written to .bashrc"
 fi
