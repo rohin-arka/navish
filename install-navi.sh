cad19400fffa3bc19349f7b0ca277d2c9eb68f5b
Sujit Shakya <sshakya.mail@gmail.com>
2017-12-08 18:12:29 +0545

    Add NAVI_ENV while installing navi command

--- install-navi.sh
+++ install-navi.sh
@@ -8,12 +8,14 @@ cp bin/navi.sh ~/.navi/bin/navi
 # shell = $SHELL
 if [ "$SHELL"="/bin/zsh" ]; then
     echo "export NAVI_PATH=\$HOME/.navi" >> ~/.zshrc
+    echo "export NAVI_ENV=local" >> ~/.zshrc
     echo "export PATH=\$PATH:\$NAVI_PATH/bin" >> ~/.zshrc
 
     echo "use `navi install` command to install navi-localLockbox"
     zsh
 else
     echo "export NAVI_PATH=\$HOME/.navi" >> ~/.bashrc
+    echo "export NAVI_ENV=local" >> ~/.bashrc
     echo "export PATH=\$PATH:\$NAVI_PATH/bin" >> ~/.bashrc
 
     echo "use `navi install` command to install navi-localLockbox"
